23ff31fc416752cf820dca0f5339ab7863199030
Taylor Allen <taylorallen0913@gmail.com>
2023-04-03 23:34:14 -0700

    configured icon and splash screen

---
+++ app.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'GPT Mobile',
+  slug: 'gpt-mobile',
+  version: process.env.PROJECT_VERSION || '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/logo.png',
+  splash: {
+    image: './assets/logo.png',
+    backgroundColor: '#011638',
+  },
+};
